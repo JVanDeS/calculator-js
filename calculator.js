d7e439a0f7a261ddbea002bc6aa632eb0592c0c7
GH0ST\joeri <99040654@mydavinci.nl>
2017-01-30 14:20:24 +0100

    Starting again

--- calculator.js
+++
@@ -1,6 +0,0 @@
-//nummer in het scherm
-function addNumber(i) {
-	var display = document.getElementsByClassName('display')[0];
-	display.innerHTML = display.innerHTML + i;
-	number2 = display.innerHTML;
-}
